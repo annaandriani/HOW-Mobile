8d08dd31d066c2529f7ec536dfaf933a6b8e2c76
annaandriani <anninhaandriani@gmail.com>
2022-07-20 22:43:16 -0300

    add native functionality

--- app/src/main/java/com/example/movietheater/MainActivity.java
+++ app/src/main/java/com/example/movietheater/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.movietheater;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.movietheater.movies.MainFragment;
 
@@ -11,10 +13,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        setTheme(R.style.Theme_MovieTheater);
         setContentView(R.layout.activity_main);
 
         if(savedInstanceState == null){
             getSupportFragmentManager().beginTransaction().replace(R.id.frame_main, new MainFragment()).commit();
         }
+    }
+    public void chamar_camera(View view) {
+        Intent camera = new Intent(this, CameraActivity.class);
+        startActivity(camera);
     }
 }
